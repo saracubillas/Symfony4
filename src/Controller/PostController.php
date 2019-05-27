03767ac432575f0e476fd8b87661da05138ebbe6
SaraCubillas <saracubillas@gmail.com>
2019-05-27 16:07:03 +0200

    adding twig inheritance

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PostController
+class PostController extends AbstractController
 {
     /**
      * @Route()
      */
     public function home()
     {
-        return new Response('Hello tech yogi friends!!');
+        return new Response('hello tech yogi friends!!');
+    }
+
+    /**
+     * @Route("/blog/{post}")
+     */
+    public function showPost($post)
+    {
+        return $this->render('posts/post.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $post))
+        ]);
     }
 
 }
